a79d8ea2e01422a26990974ca9a8cec2ef001f5c
rakhazan <rakhazahnur26@gmail.com>
2023-10-19 06:32:24 +0700

    :pencil2: fix komunitas redirect when logged in

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ const Logout = () => {
 document.addEventListener('DOMContentLoaded', function () {
     const loggedInNav = document.getElementById('loggedIn')
     const nloggedInNav = document.getElementById('nloggedIn')
+    const komunitasNav = document.getElementById('komunitas')
 
     const isLogin = sessionStorage.getItem('isLogin') === 'true' || false
     if (isLogin) {
         loggedInNav.style.display = 'block'
         nloggedInNav.style.display = 'none'
+        komunitasNav.setAttribute('href', './diskusi-list.html')
     }
 })
